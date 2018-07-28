8b5eb7d7220259ef439ebc99b47e26e8c5f5116a
wyx <wyx@163.com>
2018-07-28 13:27:28 +0800

    完善demo

--- demo/coroutine/redis.php
+++ demo/coroutine/redis.php
@@ -1,5 +1,22 @@
 <?php
-$redis = new Swoole\Coroutine\Redis();
-$redis->connect('127.0.0.1', 6379);
-$redis->set('key', 'val');
-echo $redis->get('key');
+/**
+ * Created by PhpStorm.
+ * User: baidu
+ * Date: 18/3/19
+ * Time: 上午1:24
+ */
+
+$http = new swoole_http_server('0.0.0.0', 8001);
+
+$http->on('request', function($request, $response) {
+    // 获取redis 里面 的key的内容， 然后输出浏览器
+
+    $redis = new Swoole\Coroutine\Redis();
+    $redis->connect('127.0.0.1', 6379);
+    $value = $redis->get($request->get['a']);
+
+    $response->header("Content-Type", "text/plain");
+    $response->end($value);
+});
+
+$http->start();
